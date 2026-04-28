f68a26982de2c67a77ca745edd271f0b2b85e897
Nithin R <nithinr123n8@gmail.com>
2026-04-28 21:21:23 +0530

    feat: build caretaker registration with parent ID generation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage.jsx'
 import AuthPage from './pages/AuthPage.jsx'
+import CaretakerRegistered from './pages/CaretakerRegistered.jsx'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/auth" element={<AuthPage />} />
+      <Route path="/caretaker/registered" element={<CaretakerRegistered />} />
     </Routes>
   )
 }
